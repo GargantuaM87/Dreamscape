4db982d12334736151a988e465d620cc8a0c20df
Ayinde Abrams <xxxenouniverse@gmail.com>
2025-09-23 22:07:10 -0400

    Documention to Enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,8 +12,6 @@ public class Enemy : MonoBehaviour, IHitable
     private Rigidbody rb;
     private Bells bellSystem;
 
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,7 +20,6 @@ public class Enemy : MonoBehaviour, IHitable
         matController = new MaterialController(mats[0], mats[1], mats[2]);
         bellSystem = FindAnyObjectByType<Bells>();
     }
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("PDamage"))
@@ -32,6 +29,9 @@ public class Enemy : MonoBehaviour, IHitable
             //Enemy burst after dying with screen shake
         }
     }
+    /// <summary>
+    /// Visual Effects for when an enemy is damaged.
+    /// </summary>
     public void HitSplash()
     {
         audioManager.PlaySound("HitEffect");
@@ -39,15 +39,22 @@ public class Enemy : MonoBehaviour, IHitable
         transform.DOShakeScale(0.4f, 0.5f, 1, 2, true, ShakeRandomnessMode.Harmonic);
         matController.DOColor(Color.white);
     }
-
+    /// <summary>
+    /// Called externally to provide information such as the force of knock back.
+    /// </summary>
+    /// <param name="executeSource"></param>
+    /// <param name="knockbackForce"></param>
     public void Execute(Transform executeSource, int knockbackForce)
     {
         KnockbackEntity(executeSource, knockbackForce);
     }
-
     public void DecreaseWaveLength() => parentWave.EnemyLength -= 1;
     public void IncreaseSomni() => bellSystem.GainSomni();
-
+    /// <summary>
+    /// Calculations for executing knock back logic on an enemy through physics.
+    /// </summary>
+    /// <param name="executionSource"></param>
+    /// <param name="knockbackForce"></param>
     public void KnockbackEntity(Transform executionSource, int knockbackForce)
     {
         Vector3 dir = (transform.position - executionSource.transform.position).normalized;
@@ -58,7 +65,6 @@ public class Enemy : MonoBehaviour, IHitable
            pos.z += dir.z * knockbackForce;
          transform.position = new Vector3(pos.x, transform.position.y, pos.z);*/
     }
-
     public float Priority { get { return priority; } set { priority = value; } }
     public Waves ParentWave { get { return parentWave; } set { parentWave = value; } }
 }
